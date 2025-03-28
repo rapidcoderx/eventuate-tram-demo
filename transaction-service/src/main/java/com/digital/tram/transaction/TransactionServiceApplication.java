66421865fe23be25fec370ec6f1b66439eb646a4
rapidcoderx <nitishvsrm@gmail.com>
2025-03-28 15:56:25 -0600

    Code refactor.

--- transaction-service/src/main/java/com/digital/tram/transaction/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/digital/tram/transaction/TransactionServiceApplication.java
@@ -1,9 +1,15 @@
 package com.digital.tram.transaction;
 
+import io.eventuate.tram.events.common.DefaultDomainEventNameMapping;
+import io.eventuate.tram.events.common.DomainEventNameMapping;
+import io.eventuate.tram.messaging.common.ChannelMapping;
+import io.eventuate.tram.messaging.common.DefaultChannelMapping;
 import io.eventuate.tram.spring.consumer.jdbc.TramConsumerJdbcAutoConfiguration;
 import io.eventuate.tram.spring.consumer.kafka.EventuateTramKafkaMessageConsumerConfiguration;
 import io.eventuate.tram.spring.events.publisher.TramEventsPublisherConfiguration;
+import io.eventuate.tram.spring.events.subscriber.TramEventSubscriberConfiguration;
 import io.eventuate.tram.spring.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,19 +21,30 @@ import org.springframework.web.reactive.function.client.WebClient;
   TramMessageProducerJdbcConfiguration.class,
   TramConsumerJdbcAutoConfiguration.class,
   EventuateTramKafkaMessageConsumerConfiguration.class,
-  TramEventsPublisherConfiguration.class
+  TramEventsPublisherConfiguration.class,
+  TramEventSubscriberConfiguration.class
 })
 public class TransactionServiceApplication {
+  @Value("${balance.service.url:http://localhost:8081}")
+  private String balanceServiceUrl;
+
   public static void main(String[] args) {
     SpringApplication.run(TransactionServiceApplication.class, args);
   }
 
   @Bean
   public WebClient.Builder webClientBuilder() {
-    String balanceServiceUrl = System.getenv("BALANCE_SERVICE_URL");
-    if (balanceServiceUrl == null || balanceServiceUrl.isEmpty()) {
-      balanceServiceUrl = "http://localhost:8081"; // Default for local development
-    }
     return WebClient.builder().baseUrl(balanceServiceUrl);
+  }
+
+  // Add these beans if they're not provided by imports
+  @Bean
+  public ChannelMapping channelMapping() {
+    return new DefaultChannelMapping.DefaultChannelMappingBuilder().build();
+  }
+
+  @Bean
+  public DomainEventNameMapping domainEventNameMapping() {
+    return new DefaultDomainEventNameMapping();
   }
 }
